af3ec1db7561e12613256f33f747859c538894af
= <121827489+Icaredeco@users.noreply.github.com>
2025-02-01 20:25:41 +0100

    Init

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ async def generate_image():
     prompt = data.get('prompt', 'Une créature chibi mignonne avec de grands yeux et des ailes de fée, style anime')
     
     # Récupération de la clé API Replicate depuis une variable d'environnement
-    replicate_api_token = os.environ.get("r8_ds0MnEFtu9uCSV03LkR9IJCsAV05c9X3Z7uhg")
+    replicate_api_token = os.environ.get("REPLICATE_API_TOKEN")
     if not replicate_api_token:
         return jsonify({"error": "Clé API Replicate manquante"}), 500
     os.environ["REPLICATE_API_TOKEN"] = replicate_api_token
